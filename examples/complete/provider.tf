6a0e3147ba4a9a304d78275fb1835f09b318ce9c
devops0706 <devops@uventure.in>
2024-02-26 13:48:00 +0530

    updated provider.tf

--- examples/complete/provider.tf
+++ examples/complete/provider.tf
@@ -6,11 +6,11 @@ provider "aws" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = "test-aj-eks"
+  name = ""
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = "test-aj-eks"
+  name = ""
 }
 
 provider "kubernetes" {
